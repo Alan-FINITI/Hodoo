3cc2f48d2f3fd5c322b617c19959a73ef0d7a9ce
Hallebanc <albancharrin.c@gmail.com>
2025-03-27 08:47:45 +0100

    test client

--- models/refugeClient.py
+++ models/refugeClient.py
@@ -1,23 +1,32 @@
 from odoo import api, models, fields
 
 
-class ClientWeb(models.Model):
-    _name = "refuge.clientWeb"
-    _description = "Client Web"
+class RefugeClient(models.Model):
+    _name = "refuge.client"
+    _description = "Client pour l'application web"
     _inherit_ = "res.partner"
 
-    fidelity_points = fields.Integer(string="Points de fidélité")
-
-    discount = fields.Float(string = "Reduction totale", compute="calculate_discount", default=1)
-
+    fidelity_points = fields.Integer(string="Points de fidélité", compute="calculate_fidelity_points")
+    discount = fields.Float(string="Réduction totale", compute="calculate_discount", default=1)
+    order_ids = fields.One2many(string="Order Ids", comodel_name="refuge.order", inverse_name="client_id")
     @api.model
     def calculate_discount(self):
-        if self.fidelity_points >= 1000:
-            self.discount = 0.8
-        elif self.fidelity_points >= 500:
-            self.discount = 0.85
-        elif self.fidelity_points >= 200:
-            self.discount = 0.90
-        elif self.fidelity_points >= 100:
-            self.discount = 0.95
+        for record in self:
+            if record.fidelity_points >= 1000:
+                record.discount = 0.8
+            elif record.fidelity_points >= 500:
+                record.discount = 0.85
+            elif record.fidelity_points >= 200:
+                record.discount = 0.90
+            elif record.fidelity_points >= 100:
+                record.discount = 0.95
+
+    def calculate_fidelity_points(self):
+        for record in self:
+            total_points = 0
+            for order in record.order_ids:
+                total_points += int(order.amount_total)
+            record.fidelity_points = total_points
+
+
 
