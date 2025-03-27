37411f10ef5e1b4b9c7ca674b69c4b9f13a2681b
Hallebanc <albancharrin.c@gmail.com>
2025-03-27 11:47:58 +0100

    test

---
+++ static/src/app/screens/bag_command/bag_command.js
@@ -0,0 +1,30 @@
+import { registry } from "@web/core/registry";
+import { useRefuge } from "@refuge_aventuriers/app/store/refuge_hook";
+import { Component } from "@odoo/owl";
+
+export class BagCommandScreen extends Component {
+    static template = "refuge_aventuriers.BagCommandScreen";
+
+    setup() {
+        this.refuge = useRefuge();
+    }
+
+
+    onInput(ev, field) {
+        this.props.order[field] = ev.target.value;
+        this.refuge.orm.call("refuge.order", "write", [this.props.order.id, this.props.order])
+    }
+
+    on_order_button_click(field, value){
+    this.refuge.orm.call("refuge.order", "write", [
+                this.props.order.id,
+                {
+                    state: "Envoyé",
+                }
+            ]);
+    }
+
+
+}
+
+registry.category("refuge_screens").add("BagCommandScreen", BagCommandScreen);
